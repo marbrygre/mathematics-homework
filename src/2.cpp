1027e39953a5562fa72ca922f333891c45df523e
marbrygre <marbry.gre@gmx.com>
2025-03-05 17:14:23 +0000

    Add 2.cpp

---
+++ src/2.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x = 5; // change this value to test different scenarios
+    if (x % 2 == 0) {
+        cout << "The number is even." << endl;
+    } else {
+        cout << "The number is odd." << endl;
+    }
+    return 0;
+}
